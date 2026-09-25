281db4d35fc19757a6d61f373d4caff037475968
vdgizmo <vdcadastros@outlook.com>
2026-09-25 14:20:00 -0300

    Add files via upload

--- recursos/scripts/menu_overlay.js
+++ recursos/scripts/menu_overlay.js
@@ -5,3 +5,19 @@ function abreJogador() {
 function fechaJogador() {
   document.getElementById("navJogador").style.height = "0%";
 }
+
+function abreMestre() {
+  document.getElementById("navMestre").style.height = "100%";
+}
+
+function fechaMestre() {
+  document.getElementById("navMestre").style.height = "0%";
+}
+
+function abreCenario() {
+  document.getElementById("navCenario").style.height = "100%";
+}
+
+function fechaCenario() {
+  document.getElementById("navCenario").style.height = "0%";
+}
